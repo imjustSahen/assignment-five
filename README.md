2fdd8f26947390810a52d40152c477c41da7e988
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2022-11-22 16:00:19 -0800

    updated JS and README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# assignment-five
+# Third-Party API's
+
+## Project Title
+
+Work Day Scheduler
+
+## Description
+
+Assignment Five was to add functionality to the provided HTML and CSS files. By creating some Div containers to represent each hour of the work day and creating logic for users to add tasks or goals to each individual hour of their work day. This project used DAYJS to add the date and to check what hour of the day it is to show the user what hour they are currently in. When the user adds a task or goal and clicks the 'Save' icon the data is stored in Local Storage. I am working to add the ability for the user to double click the same icon to remove the task or goal as well.
+
+## Links
+
+- Website: https://imjustsahen.github.io/assignment-five/
+- Repo: https://github.com/imjustSahen/assignment-five
+
+## Installation
+
+N/A
+
+## Usage
+![image](assets\images\Work-Day-Scheduler.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,72 @@
-var currentDate = dayjs().format('MMMM D, YYYY');
-$('#currentDate').text(currentDate);
+// Add date to header
+const currentDate = dayjs().format("MMMM D, YYYY");
+$("#currentDate").text(currentDate);
 
+function trackTime() {
+  // Get current hour
+  const timeNow = dayjs().hour();
+
+  $(".time-block").each(function () {
+    const blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // Adjust background colors depending on past, present, & future
+    if (blockTime < timeNow) {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+$(function () {
+  // Event listener
+  $(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    let hour = $(this).parent().attr("id");
+    let description = $(this).siblings(".description").val();
+
+    // Save schedule to local storage
+    localStorage.setItem(hour, description);
+  });
+
+  // Get description from local storage
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  trackTime();
+});
+
+// $(function () {
+//   $(function () {
+//     $(".saveBtn").on("dblclick", function () {
+//       localStorage.removeItem(hour, description);
+//     });
+
+//     $("#hour8 .description").val(localStorage.removeItem("hour8"));
+//     $("#hour9 .description").val(localStorage.removeItem("hour9"));
+//     $("#hour10 .description").val(localStorage.removeItem("hour10"));
+//     $("#hour11 .description").val(localStorage.removeItem("hour11"));
+//     $("#hour12 .description").val(localStorage.removeItem("hour12"));
+//     $("#hour13 .description").val(localStorage.removeItem("hour13"));
+//     $("#hour14 .description").val(localStorage.removeItem("hour14"));
+//     $("#hour15 .description").val(localStorage.removeItem("hour15"));
+//     $("#hour16 .description").val(localStorage.removeItem("hour16"));
+//     $("#hour17 .description").val(localStorage.removeItem("hour17"));
+//   });
+// });
